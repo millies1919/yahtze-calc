27db13ae7cd6b9bbf37da93bffd3eeebb3e1b42a
Matt Illies <mattillies19@gmail.com>
2019-12-26 12:11:33 -0600

    added top field logic, an bottom fields

--- index.js
+++ index.js
@@ -1,52 +1,34 @@
 var topTotal = 0;
 var finalTop = 0;
-var onesTotal = 0;
-var twosTotal = 0;
-var threesTotal = 0;
-var foursTotal = 0;
-var fivesTotal = 0;
-var sixesTotal = 0;
+var previouslyEntered = 0;
 
-var ones = document.querySelector('#onesbox');
-var twos = document.querySelector('#twosbox');
-var threes = document.querySelector('#threesbox');
-var fours = document.querySelector('#foursbox');
-var fives = document.querySelector('#fivesbox');
-var sixes = document.querySelector('#sixesbox');
+var tops = Array.from(document.querySelectorAll('.topfield'));
 var totalTop = document.querySelector('#totaltop');
 
-function oneChange(e) {
-  onesTotal = e.target.value;
+function onChange(e) {
+    if(typeof(parseInt(e.target.value)) !== NaN) {
+        if(parseInt(e.target.value) !== previouslyEntered) {
+        topTotal = (topTotal - previouslyEntered) + parseInt(e.target.value)
+        previouslyEntered = parseInt(e.target.value)
+        finalTop = topTotal > 62 ? topTotal + 35 : topTotal;
+        totalTop.textContent =`${finalTop}`;
+        } else {
+        topTotal = parseInt(e.target.value) + topTotal;
+        previouslyEntered = parseInt(e.target.value);
+        finalTop = topTotal > 62 ? topTotal + 35 : topTotal;
+        totalTop.textContent =`${finalTop}`;
+        }
+    } else {
+        alert("Please enter a number")//ask fel on this one
+    }
 }
 
-function twoChange(e) {
-  twosTotal = e.target.value;
+function onClick(e) {
+    previouslyEntered = e.target.value;
 }
 
-function threeChange(e) {
- threesTotal = e.target.value;
-}
+tops.map(item => {
+    item.addEventListener('change', onChange)
+    item.addEventListener('click', onClick)
+})
 
-function fourChange(e) {
- foursTotal = e.target.value;
-}
-
-function fiveChange(e) {
-  fivesTotal = e.target.value;
-}
-
-function sixChange(e) {
-  sixesTotal = e.target.value;
-}
-
-ones.addEventListener('change', oneChange);
-twos.addEventListener('change', twoChange);
-threes.addEventListener('change', threeChange);
-fours.addEventListener('change', fourChange);
-fives.addEventListener('change', fiveChange);
-sixes.addEventListener('change', sixChange);
-
-topTotal = onesTotal + twosTotal + threesTotal + foursTotal + fivesTotal + sixesTotal;
-console.log(topTotal)
-finalTop = topTotal > 62 ? topTotal + 35 : topTotal;
-totalTop.textContent =`${finalTop}`;
